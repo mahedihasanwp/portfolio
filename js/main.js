4942df176328c01f846ae0b0f8c270f5dadf9f89
mahedihasanwp <40929864+mahedihasanwp@users.noreply.github.com>
2018-07-08 00:21:42 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+          $(document).ready(function(){
+            $('.html').animate({width:'90%'},2000);
+            $('.css').animate({width:'75%'},2000);
+            $('.JavaSc').animate({width:'50%'},2000);
+            $('.bootstrap').animate({width:'65%'},2000);
+            $('.php').animate({width:'70%'},2000);
+            $('.ajax').animate({width:'45%'},2000);
+            $('.photoshop').animate({width:'60%'},2000);
+            $('.illustrator').animate({width:'40%'},2000);
+          });
+        
